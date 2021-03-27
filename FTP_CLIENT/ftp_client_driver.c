ca4769cb874b3d2a6c28ccd1f9cd94f4df72962d
angkokleong <angkokleong26@gmail.com>
2021-03-27 13:06:03 +0800

    Added basic skeleton function using ftp_client.c in ftp_client_driver.c

--- FTP_CLIENT/ftp_client_driver.c
+++ FTP_CLIENT/ftp_client_driver.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void ftp_client_driver(char *ip_address_or_hostname){
+int ftp_client_driver(char *ip_address_or_hostname){
 	
-	printf("TEST VALUE: %s", ip_address_or_hostname);	
+	FTP_CLIENT_INFORMATION ftp_client_information;
+	ftp_client_information = construct_ftp_client_information();
+
+	int port_number = generate_port_number();
 	
+	open_socket_ipv4(&ftp_client_information->socket_file_descriptor);
+	
+	set_server_address_ipv4(ftp_client_information);
+
+	setup_ftp_connection_ipv4(ftp_client_information);
+
+	//write function
+
+	close_socket(ftp_client_information.socket_file_descriptor);
+	//printf("TEST VALUE: %s", ip_address_or_hostname);	
+	return EXIT_FAILURE;	
 }
 
